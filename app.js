9f47eb4864e6328ece95e65dfdb162a54edf2339
Huynh Tuan Anh <huynhtuananh21895@gmail.com>
2018-08-05 13:02:40 +0700

    implement service for multiple users

--- app.js
+++ app.js
@@ -11,58 +11,63 @@ app.use(bodyParser.json());
 const base = new Airtable({apiKey: configs.airtable.apiKey}).base("apprdRz3g2BNFzkj6")(configs.airtable.tableName);
 
 app.get('/', (req, res) => {
-  console.log('Testing server logs');
-  
   base.select({
+    fields: ['Phone'],
     view: 'Grid view'
-  }).firstPage((err, records) => {
+  }).firstPage((err, users) => {
     if (err) {
       res.status(500).json({err});
     } else {
-      res.status(200).json({message: 'Fetch records successfully', records});
+      res.status(200).json({message: 'Fetch records successfully', users});
     }
   });
 });
 
-app.post('/sms', (req, res) => {
-  let SUCCESS_CODE = "00";
-  let speedSmsOptions = {
-    url: 'https://api.speedsms.vn/index.php/sms/send',
-    auth: {
-      'user': configs.speedsmsAccessToken,
-      'pass': '123@123a'
-    },
-    headers: { 'Content-Type': 'application/json' },
-    json: req.body
-  };
+app.get('/send-sms', (req, res) => {
+  base.select({
+    fields: ['Phone'],
+    view: 'Grid view'
+  }).firstPage((err, users) => {
+    if (err) {
+      res.status(500).json({err});
+    } else {
+      phoneNumbers = configs.getAllPhoneNumbers(users);
+      let { options, SUCCESS_CODE } = configs.speedSms;
 
-  request.post(
-    speedSmsOptions,
-    (err, response, body) => {
-      if (err) {
-        res.status(500).json({message: 'Send sms failed', err});
-      } else if (body.code !== SUCCESS_CODE) {
-        res.status(500).json(body);
-      } else {
-        base.update(
-          'recjHFIel90qnQhdY', 
-          {'Đã gửi SMS': true}, 
-          (err, updatedRecord) => {
-            if (err) {
-              res.status(500).json({err});
-            } else {
-              res.status(200).json({message: 'Sms sent and sending-status updated successfully', body, updatedRecord});
-            }
+      // get all user phone numbers first then create request to speedsms.vn to send sms to those people
+      options.json.to = phoneNumbers;
+
+      request.post(
+        options,
+        (err, response, body) => {
+          if (err) {
+            res.status(500).json({message: 'Send sms failed', err});
+          } else if (body.code !== SUCCESS_CODE) {
+            res.status(500).json(body);
+          } else {
+            userIds = configs.getAllUserIds(users);
+
+            // update status of multiple users
+            userIds.forEach(id => {
+              base.update(
+                id, 
+                {'Đã gửi SMS': true}, 
+                (err, updatedRecord) => {
+                  if (err) {
+                    res.status(500).json({err});
+                  }
+                }
+              );
+            });
+            res.status(200).json({message: 'Sms sent and sending-status updated successfully', body});
           }
-        );
-      }
+        }
+      );
     }
-  );
+  });
 });
 
 app.post('/sms-reply', (req, res) => {
-  console.log(req.body);
-  
   userPhone = configs.standardisePhoneNumber(req.body.phone);
 
   base.select({
@@ -70,7 +75,7 @@ app.post('/sms-reply', (req, res) => {
     view: 'Grid view'
   }).firstPage((err, users) => {
     if (err) {
-      res.status(500).json({message: "Failed to fetch users", err});
+      res.status(500).json({message: "Failed to fetch user", err});
     } else if (users.length === 0) {
       res.status(404).json({message: 'User not found'});
     } else {
--- configs.js
+++ configs.js
@@ -5,11 +5,49 @@ const standardisePhoneNumber = (phone) => {
   return phone
 };
 
+const getAllPhoneNumbers = (users) => {
+  let phoneNumbers = [];
+  users.forEach((user) => {
+    phoneNumbers.push(user.fields.Phone);
+  });
+
+  return phoneNumbers;
+}
+
+const getAllUserIds = (users) => {
+  let ids = [];
+  users.forEach((user) => {
+    ids.push(user.id);
+  });
+
+  return ids;
+}
+
+const speedSmsOptions = {
+  url: 'https://api.speedsms.vn/index.php/sms/send',
+  auth: {
+    'user': 'ltHNv_80TkJw07pYNsA84cdx6RND0lDR',
+    'pass': '123@123a'
+  },
+  headers: { 'Content-Type': 'application/json' },
+  json: {
+    "to": [],
+    "content": "testing speedsms.vn from Tuan Anh Huynh",
+    "sms_type": 2,
+    "sender": ""
+  }
+};
+
 module.exports = {
   airtable: {
     apiKey: 'keylH089x65GF9EME',
     tableName: 'Table 1'
   },
+  speedSms: {
+    options: speedSmsOptions,
+    SUCCESS_CODE: '00'
+  },
   standardisePhoneNumber,
-  speedsmsAccessToken: 'ltHNv_80TkJw07pYNsA84cdx6RND0lDR'
+  getAllPhoneNumbers,
+  getAllUserIds
 }
